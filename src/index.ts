5e5f109d72e3f0923cec51f32a5f18c06acc4e45
Daniel Balogh <danielferencortel@gmail.com>
2024-12-22 22:25:51 +0200

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ export {
   getPropertyInInheritance,
   getMethodInInheritance
 } from './reduce-value/properties';
-export { externalsToWritableDeclarations } from './write-utils/handle-externals';
-export { writeReducedDeclaration } from './write-utils/writers';
+export { externalsToWritableDeclarations} from './write-utils/handle-externals';
+export { writeReducedDeclaration, writeLocalStatements } from './write-utils/writers';
 export { writeExternals, writeImports, writeStatements } from './write-utils/write-externals';
 export { getFunctionDeclaration, getExternalIdentifier } from './utils';
 export * from './types';
